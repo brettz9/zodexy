21cc6347092d5c874ebe4369392820b598b89b60
Brett Zamir <brettz9@yahoo.com>
2026-08-19 17:04:32 -0700

    refactor: prettier

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -3,19 +3,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import {registerTemplate, Template} from '@webcoder49/code-input';
+import { registerTemplate, Template } from "@webcoder49/code-input";
 
-import Indent from '@webcoder49/code-input/plugins/indent.mjs';
+import Indent from "@webcoder49/code-input/plugins/indent.mjs";
 
-import 'prismjs/themes/prism.min.css';
-import'@webcoder49/code-input/code-input.min.css';
+import "prismjs/themes/prism.min.css";
+import "@webcoder49/code-input/code-input.min.css";
 
 import { App } from "./App";
 import "./index.css";
 
 registerTemplate(
-  'syntax-highlighted',
-  new Template(Prism.highlightElement, false, true, false, [new Indent()])
+  "syntax-highlighted",
+  new Template(Prism.highlightElement, false, true, false, [new Indent()]),
 );
 
 createRoot(document.getElementById("root")!).render(
